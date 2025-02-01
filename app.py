f6b2dfeba3d31b8af15c6a8ae6b7dfb9ad408a13
quimsonchin <k_quimso@live.concordia.ca>
2025-02-01 15:00:47 -0500

    html file
    ignore views error

--- app.py
+++ app.py
@@ -1,9 +1,8 @@
 from flask import Flask
-app = Flask(__name__)
+from views import views
 
-@app.route('/')
-def home():
-    return "this is the home page. We gna win hackathon baby!!!"
+app = Flask(__name__)
+app.register_blueprint(views, url_prefix="/views")
 
 if __name__ == '__main__':
-    app.run(debug=True,port=8000)
+    app.run(debug=True, port=8000)
--- views.py
+++ views.py
@@ -1,1 +1,11 @@
+<<<<<<< Updated upstream
 print("hello")
+=======
+from flask import Blueprint
+
+views = Blueprint("views")
+
+@views.route('/')
+def home():
+    return "home page"
+>>>>>>> Stashed changes
