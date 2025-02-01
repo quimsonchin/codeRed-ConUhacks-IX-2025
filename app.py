e9fbf8d0f0a9d3b8c1a2948c8e6cbfed45a15518
MicoLosloso <micojohnlosloso@gmail.com>
2025-02-01 14:32:20 -0500

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "this is the home page"
+    return "this is the home page. We gna win hackathon baby!!!"
 
 if __name__ == '__main__':
     app.run(debug=True,port=8000)
