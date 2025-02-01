866fbd20c86974eb9e86a82a1ceb8f07dd93a266
Selma Bédoui <selmabeds@gmail.com>
2025-02-01 14:54:35 -0500

    Update user.py

--- user.py
+++ user.py
@@ -1,1 +1,10 @@
+#This file is used to sign in a user
+#Avoids giving everyone the same advice
 
+#Tuple storing user data
+#Explanation: If user enters user1 and password1, acess granted until changed
+users = [
+    ("user1"; "password1")
+    ("user2"; "password2")
+    ("user3"; "password3")
+]
